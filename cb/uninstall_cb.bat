ef00614c7ddbe5ca001f95dd059913b4cd7aec18
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2022-02-09 05:55:29 +0530

    Delete cb directory

--- cb/uninstall_cb.bat
+++
@@ -1,8 +0,0 @@
-#Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass
-
-@echo off
-
-"C:\Program Files\Confer\uninstall.exe" /uninstall
-echo.
-echo  ** Carbon Black (CB) Sensor Uninstalled Successfully ** 
-echo.
